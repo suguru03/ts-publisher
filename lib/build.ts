dcceed1c188a76fb5c025fe3db9a01b441099b86
Suguru Motegi <suguru.motegi@gmail.com>
2019-03-14 23:21:21 -0700

    feat(build): update build script

--- lib/build.ts
+++ lib/build.ts
@@ -1,4 +1,6 @@
 import * as path from 'path';
+import * as fs from 'fs';
+import { execSync } from 'child_process';
 
 import { projectPath, modulePath } from './util';
 
@@ -10,8 +12,21 @@ export const build = () => {
     throw new Error('Need to define outDir');
   }
   // TODO: delete outDir
+  const outDirPath = path.resolve(projectPath, outDir);
+
   const tscpath = path.resolve(modulePath, 'typescript', 'lib', 'tsc');
   // TODO: switch to VM?
   process.argv.splice(2, 1);
   require(tscpath);
+
+  // update bin files
+  const binpath = path.join(outDirPath, 'bin');
+  if (fs.existsSync(binpath) && fs.statSync(binpath).isDirectory()) {
+    for (const file of fs.readdirSync(binpath)) {
+      const filepath = path.join(binpath, file);
+      if (/.js$/.test(file)) {
+        execSync(`chmod 755 ${filepath}`);
+      }
+    }
+  }
 };
