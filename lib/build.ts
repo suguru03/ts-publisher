04cff344570909ce511e371bd42289dff0960413
Suguru Motegi <suguru.motegi@gmail.com>
2019-03-18 00:56:20 -0700

    feat(build): add publish files

--- lib/build.ts
+++ lib/build.ts
@@ -4,6 +4,8 @@ import { execSync } from 'child_process';
 
 import { projectPath, modulePath, getOutDirPath } from './util';
 
+const files = ['./README.md'];
+
 export const build = (...args) => {
   const cmd = args[args.length - 1];
   const tscPath = path.resolve(modulePath, 'typescript', 'bin', 'tsc');
@@ -24,6 +26,16 @@ export const build = (...args) => {
   const packagePath = path.resolve(outDirPath, 'package.json');
   const indent = (packageJson.prettier && packageJson.prettier.tabWidth) || 2;
   fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, indent));
+
+  // copy files
+  // TODO: check package.files
+  for (const file of files) {
+    const filepath = path.resolve(projectPath, file);
+    if (!fs.existsSync(filepath)) {
+      continue;
+    }
+    fs.writeFileSync(path.resolve(outDirPath, file), fs.readFileSync(filepath));
+  }
 
   // update bin files
   const binpath = path.join(outDirPath, 'bin');
