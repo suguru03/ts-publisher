9b2294c222505b8a9b9e92961258011111abac7a
Suguru Motegi <suguru.motegi@gmail.com>
2019-03-17 18:28:34 -0700

    feat(build): resolve binary files

--- lib/build.ts
+++ lib/build.ts
@@ -6,27 +6,26 @@ import { projectPath, modulePath } from './util';
 
 export const build = () => {
   // TODO: add option to specify tsconfig
-  const { compilerOptions } = require(path.resolve(projectPath, 'tsconfig.json'));
+  const tsconfigPath = path.resolve(projectPath, 'tsconfig.json');
+  const { compilerOptions } = require(tsconfigPath);
   const { outDir = '' } = compilerOptions || {};
   if (!outDir) {
     throw new Error('Need to define outDir');
   }
-  // TODO: delete outDir
   const outDirPath = path.resolve(projectPath, outDir);
 
-  const tscpath = path.resolve(modulePath, 'typescript', 'lib', 'tsc');
-  // TODO: switch to VM?
-  process.argv.splice(2, 1);
-  require(tscpath);
+  const tscPath = path.resolve(modulePath, 'typescript', 'bin', 'tsc');
+  execSync(`${tscPath} --project ${projectPath}`).toString();
 
   // update bin files
   const binpath = path.join(outDirPath, 'bin');
-  if (fs.existsSync(binpath) && fs.statSync(binpath).isDirectory()) {
-    for (const file of fs.readdirSync(binpath)) {
-      const filepath = path.join(binpath, file);
-      if (/.js$/.test(file)) {
-        execSync(`chmod 755 ${filepath}`);
-      }
+  if (!fs.existsSync(binpath) || !fs.statSync(binpath).isDirectory()) {
+    return;
+  }
+  for (const file of fs.readdirSync(binpath)) {
+    const filepath = path.join(binpath, file);
+    if (/.js$/.test(file)) {
+      execSync(`chmod 755 ${filepath}`);
     }
   }
 };
--- lib/util.ts
+++ lib/util.ts
@@ -1,4 +1,7 @@
+import * as fs from 'fs';
 import * as path from 'path';
 
-export const projectPath = path.resolve(__dirname, '../../../');
+export const projectPath = ['../..', '../../..']
+  .map(p => path.resolve(__dirname, p))
+  .find(p => fs.existsSync(path.resolve(p, 'package.json')));
 export const modulePath = path.resolve(projectPath, 'node_modules');
