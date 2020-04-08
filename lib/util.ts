168107107470f18030f7a5c9de12311d95426942
Suguru Motegi <suguru.motegi@gmail.com>
2020-04-08 10:40:17 -0700

    chore(package): udpate dependencies

--- lib/util.ts
+++ lib/util.ts
@@ -2,8 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const projectPath = ['../..', '../../..']
-  .map(p => path.resolve(__dirname, p))
-  .find(p => fs.existsSync(path.resolve(p, 'package.json')));
+  .map((p) => path.resolve(__dirname, p))
+  .find((p) => fs.existsSync(path.resolve(p, 'package.json')));
 export const modulePath = path.resolve(projectPath, 'node_modules');
 
 export const getOutDirPath = (configPath: unknown) => {
